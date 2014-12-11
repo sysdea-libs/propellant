17cddfcfda3b50e13eb44fc9350fa8f78efc5ecf
Chris Spencer <chris.ag.spencer@googlemail.com>
2014-12-11 10:15:06 +0000

    Add testing method to Propellant

--- lib/propellant.ex
+++ lib/propellant.ex
@@ -84,6 +84,25 @@ defmodule Propellant do
   def stop(pid) do
     GenServer.call(pid, {:stop})
   end
+
+  def test_browser do
+    {:ok, pid} = Propellant.start_link
+
+    submenu1 = Propellant.Menu.create(pid)
+    |> Propellant.Menu.add_item(%{label: "My Label", command_id: 1})
+
+    submenu2 = Propellant.Menu.create(pid)
+    |> Propellant.Menu.add_item(%{label: "My Label 2", command_id: 2})
+
+    menu = Propellant.Menu.create(pid)
+    |> Propellant.Menu.add_submenu(%{menu_id: submenu1.id, label: "My Menu", command_id: 3})
+    |> Propellant.Menu.add_submenu(%{menu_id: submenu2.id, label: "My Other Menu", command_id: 4})
+    |> Propellant.Menu.set_application_menu
+
+    Propellant.Window.create(pid, %{root_url: "https://google.com"})
+    |> Propellant.Window.show
+    |> Propellant.Window.focus
+  end
 end
 
 defmodule EventLogger do
